94772f07c80981ff7800bb648f555ca57b51890f
david <david@DESKTOP-U0DBDTR>
2022-06-26 17:40:04 -0600

    Add project files.

---
+++ GnarLink_Blazor/Shared/Gear.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GnarLink_Blazor.Shared
+{
+    public class Gear
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Size { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string ImageUrl { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+    }
+}
